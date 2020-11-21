e0e12897fdc049065744d602dcdeeb06bc7f9fd5
ghassen120 <kahri.ghassen@esprit.tn>
2020-11-21 15:12:03 -0800

    added sign in activity

--- app/src/main/java/com/esprit/barterexchange/MainActivity.java
+++ app/src/main/java/com/esprit/barterexchange/MainActivity.java
@@ -2,13 +2,27 @@ package com.esprit.barterexchange;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Application;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SignUp.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/esprit/barterexchange/SignUp.java
@@ -0,0 +1,27 @@
+package com.esprit.barterexchange;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class SignUp extends AppCompatActivity {
+
+    ImageView backBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        backBtn = findViewById(R.id.back);
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
