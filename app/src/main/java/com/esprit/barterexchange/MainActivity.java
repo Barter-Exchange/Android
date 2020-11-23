ba9970410b9092810aae54b7194759407f80f265
ghassen120 <kahri.ghassen@esprit.tn>
2020-11-22 19:59:07 -0800

    added splash screen

--- app/src/main/java/com/esprit/barterexchange/MainActivity.java
+++ app/src/main/java/com/esprit/barterexchange/MainActivity.java
@@ -3,12 +3,38 @@ package com.esprit.barterexchange;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    //animation variables
+    Animation animTop ,animBottom;
+
+    //layout variables
+    ImageView img;
+    LinearLayout btnLay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        //apply animation
+        //load animations
+        animTop = AnimationUtils.loadAnimation(this, R.anim.top_anim);
+        animBottom = AnimationUtils.loadAnimation(this, R.anim.bottom_anim);
+
+        ImageView img = findViewById(R.id.logo);
+        LinearLayout btnLay = findViewById(R.id.btnLay);
+
+        img.setAnimation(animTop);
+        btnLay.setAnimation(animBottom);
+
     }
 }
