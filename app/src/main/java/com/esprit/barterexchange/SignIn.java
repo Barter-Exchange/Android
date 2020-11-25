4af5c9b4f7e5216687e7cf75b88ff84600839238
Slouma99 <salim.ayari@esprit.tn>
2020-11-25 21:26:21 +0100

    SignIn Profil Menu

--- app/src/main/java/com/esprit/barterexchange/SignIn.java
+++ app/src/main/java/com/esprit/barterexchange/SignIn.java
@@ -1,21 +1,13 @@
 package com.esprit.barterexchange;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class SignIn extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_in);
     }
-
-
-
 }
